1d39ad16ef8e50bffe978b066d5e6241bac0e69a
Bill Carson <35587777+wacarson@users.noreply.github.com>
2024-06-30 14:36:35 -0400

    'Fixed' width issue by subtracting a fudge factor from the received width. Added statements to check for incorrect grid size input. Styled and moved button to make it more visible. Corrected html header and added doc type. This broke the javascript so I changed giving each div a w/h to padding. Did the opacity extra credit but commented it out because I don't like how it looks.

--- etch.js
+++ etch.js
@@ -1,3 +1,9 @@
+function setup(){
+  makeGrid()
+}
+
+document.onload = setup
+
 container = document.querySelector(".container")
 n = 16
 
@@ -13,23 +19,37 @@ function makeGrid() {
 
 
 		div.addEventListener("mouseover", function (e) {
-		r = Math.floor(Math.random()*255)
-		g = Math.floor(Math.random()*255)
-		b = Math.floor(Math.random()*255)
-		color = "rgb(" + r + "," + g +"," + b + ")"
+			r = Math.floor(Math.random()*255)
+			g = Math.floor(Math.random()*255)
+			b = Math.floor(Math.random()*255)
+			color = "rgb(" + r + "," + g +"," + b + ")"
 			e.target.style.background = color
+
+/**			op = e.target.style.opacity
+			if (op == "") {
+				op = "0"
+			}
+			op = parseFloat(op) + 0.1
+			if (op > 1) {
+				op = 1
+			}
+			e.target.style.opacity = op.toString()**/
+
 		})
 
-		str = "width: " + Math.floor(w/n) + "; height: " + h/n + ";"
+		str = "padding: " + Math.floor(h/n) + "px " + "0 0 " + Math.floor((w-15)/n) + "px;"
 		div.setAttribute("style", str)
 		container.appendChild(div)
-		console.log(w, div.getAttribute("style"))
 	}
 }
 
 function removeGrid() {
 	size = prompt("Enter a number between 2 and 100 for the grid size")
 	n = parseInt(size)
+	while(n < 2 || n > 100 || isNaN(n)) {
+		size = prompt("Ivalid entry. Enter a number between 2 and 100.")
+		n = parseInt(size)
+	}
 	while(container.firstChild) {
 		container.removeChild(container.lastChild)
 	}
